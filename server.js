e8010e165aa34bf27c02bfad43d2b87f17ba5dbd
Rubi Renteria <rubirenteria43@gmail.com>
2019-10-14 19:41:02 -0600

    Information is not popping up on modular, had to do some dbugging. ajax link was not included and had empty fields for my value selection. Added a modular and trying to get information to display on the pop up.

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 
 var app = express();
 var PORT = process.env.PORT || 8080;
-//creates json application
-var jsonParser =bodyParser.json()
  
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,7 +15,7 @@ app.use(bodyParser.text({ type: 'text/html' }));
 
 // app.use(express.static("app/public"));
 
-
+require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function () {
